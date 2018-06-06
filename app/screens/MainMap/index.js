6828a3ae2e3c969399c0ce629f7bb3e23b1fd810
nick <nicnhines3@gmail.com>
2018-06-05 19:59:02 -1000

    modal working

--- app/screens/MainMap/index.js
+++ app/screens/MainMap/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Modal, TouchableHighlight, Platform, Alert } from 'react-native';
-import MapView  from 'react-native-maps';
+import { StyleSheet, View, Text, TouchableHighlight, Platform, Alert } from 'react-native';
+import  MapView, { Marker }  from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import Modal from 'react-native-modal';
 
 import googlePlacesKey from '../../config/apiKeys';
 
@@ -89,7 +90,7 @@ export default class MainMap extends Component {
 
   render() {
     const {region} = this.state;
-    const {children, renderMarker, markers} = this.props;
+    const {children, renderMarker, markers} = this.props;MapView
     return (
       <View style={styles.container}>
         <View style={styles.searchButton}>
@@ -100,90 +101,85 @@ export default class MainMap extends Component {
             <Text>Current Location</Text>
           </TouchableHighlight>
         </View>
-
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            alert('Modal has been closed');
-            navigation.navigate(MainMap)
-          }}
-        >
-        <View style={styles.searchBar}>
-          <GooglePlacesAutocomplete
-            placeholder='Search'
-            minLength={2}
-            autoFocus={false}
-            returnKeyType={'search'} 
-            listViewDisplayed='auto' 
-            fetchDetails={true}
-            renderDescription={row => row.description} 
-            onPress={(data, details = null) => { 
-              console.log(data, details);
-            }}
-            getDefaultValue={() => ''}
-            query={{
-              // available options: https://developers.google.com/places/web-service/autocomplete
-              key: 'AIzaSyBpDEjIY5zXibnvhk3rbMno4WE3UdPxAlw',
-              language: 'en', // language of the results
-              types: '(cities)' // default: 'geocode'
-            }}
-            styles={{
-              poweredContainer: {
-                backgroundColor: 'rgba(255, 255, 255, 0.1)'
-              },
-              textInputContainer: {
-                width: '100%',
-              },
-              description: {
-                fontWeight: 'bold'
-              },
-              predefinedPlacesDescription: {
-                color: '#1faadb',
-              },
-              listView: {
-                color: 'black',
-                zIndex: 16,
-                position: 'absolute'
-              }
-            }} 
-            currentLocation={true} // Will add a 'Current loregioncation' button at the top of the predefined places list
-            // currentLocationLabel="Current loca122.4324tion"
-            nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-            GoogleReverseGeocodingQuery={{
-            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-            }}
-            GooglePlacesSearchQuery={{
-              // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-              rankby: 'distance',
-              types: 'food'
-            }}
-            filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-            predefinedPlaces={[homePlace, workPlace]}
-            debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-            renderRightButton={() => <Text onPress={() => { this.setModalVisible(!this.state.modalVisible)}}>cancel</Text>}
-          />
-          </View>
-      
+          <Modal
+            isVisible={this.state.modalVisible}
+            style={styles.modal}
+            avoidKeyboard={false}
+          >
+            <View style={styles.searchBar}>
+              <GooglePlacesAutocomplete
+                minLength={2}
+                autoFocus={false}
+                returnKeyType={'search'} 
+                listViewDisplayed='auto' 
+                // fetchDetails={true}
+                //renderDescription={row => row.description} 
+                onPress={(data, details = null) => { 
+                  console.log(data, details);
+                }}
+                getDefaultValue={() => ''}
+                query={{
+                  // available options: https://developers.google.com/places/web-service/autocomplete
+                  key: 'AIzaSyBpDEjIY5zXibnvhk3rbMno4WE3UdPxAlw',
+                  language: 'en', // language of the results
+                  components: 'country:us'
+                  // types: '(cities)' // default: 'geocode'
+                }}
+                styles={{
+                  textInputContainer: {
+                    width: '100%',
+                    marginBottom: 10,
+                  },
+                  description: {
+                    fontWeight: 'bold'
+                  },
+                  predefinedPlacesDescription: {
+                    color: 'white',
+                  },
+                  listView: {
+                    zIndex: 16,
+                    position: 'absolute',
+                    margin: 10,
+                    color: 'white'
+                  }
+                }} 
+                currentLocation={true} // Will add a 'Current loregioncation' button at the top of the predefined places list
+                // currentLocationLabel="CL"
+                nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+                GoogleReverseGeocodingQuery={{
+                // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+                }}
+                GooglePlacesSearchQuery={{
+                  // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                  rankby: 'distance',
+                  types: 'food'
+                }}
+                filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+                predefinedPlaces={[homePlace, workPlace]}
+                debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+                renderRightButton={() => <Text onPress={() => { this.setModalVisible(!this.state.modalVisible)}}>X</Text>}
+              />
+            </View>
           </Modal>
-
-            
-            
         <MapView
-        showsUserLocation
-        ref={ map => {this.map = map}}
-        style={styles.map}
-        textStyle={{ color: '#bc8b00'}}
-        containerStyle={{backgroundColor: 'white', borderColor: '#BC8B00',}}
-        data={markers}
-        initialRegion={initialRegion}
-        onRegionChange={() => this.onRegionChange.bind(this)}
-        onRegionChangeComplete={this.onRegionChangeComplete}
-        renderMarker={renderMarker}
-        onMapReady={this.onMapReady}
-        showsMyLocationButton={false}
-      />
+          showsUserLocation
+          ref={ map => {this.map = map}}
+          style={styles.map}
+          data={markers}
+          initialRegion={initialRegion}
+          onRegionChange={() => this.onRegionChange.bind(this)}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+          renderMarker={renderMarker}
+          onMapReady={this.onMapReady}
+          showsMyLocationButton={false}
+        >
+          <Marker
+            coordinate={{
+              latitude: 21.2911,
+              longitude: -157.8435
+            }}
+            title="Ala Moana" />
+        </MapView>
       </View>
     );
   }
@@ -209,23 +205,17 @@ const styles = StyleSheet.create({
   },
   searchButton: {
     flex: 1,
-    justifyContent: 'flex-start',
+    // justifyContent: 'flex-start',
     alignItems: 'center',
     marginTop: 40,
-    marginBottom: 520,
-    height: 55,
-    width: 350,
+    marginBottom: 510,
+    height: 75,
+    width: 370,
     borderStyle: 'solid',
-    backgroundColor: 'white',
-    borderRadius:2,
-    shadowColor: '#000',
-    shadowOffset: { width: 55, height: 350},
-    elevation: 1,
-    shadowRadius: 2,
-    shadowOpacity: 0.8,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderBottomWidth: 0,
-    
+    backgroundColor: 'white'
+  },
+  modal: {
+    // marginTop: -550,
+    // position: 'absolute'
   }
 });
